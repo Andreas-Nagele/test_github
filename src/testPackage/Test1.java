caff64d7ae852e098645bdfece60cae294175009
Andreas-Nagele <Andreas.Nagele@hak-reutte.ac.at>
2024-01-15 10:29:51 +0100

    create output message

--- src/testPackage/Test1.java
+++ src/testPackage/Test1.java
@@ -3,7 +3,9 @@ package testPackage;
 public class Test1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("Das ist ein Test!!!");
+		System.out.println("Das ist ebenfalls ein Test!!");
 
 	}
 
