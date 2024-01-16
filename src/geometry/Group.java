2824a4c8629ef4b765b05e01df9935a16ce6d65d
Andreas-Nagele <Andreas.Nagele@hak-reutte.ac.at>
2024-01-16 09:44:59 +0100

    Create class Group
    Generate constructor, getter, setter

---
+++ src/geometry/Group.java
@@ -0,0 +1,22 @@
+package geometry;
+
+import java.util.ArrayList;
+
+public class Group {
+	
+	private ArrayList <Shape> shapes;
+
+	public Group(ArrayList<Shape> shapes) {
+		this.shapes = new ArrayList<Shape>();
+	}
+
+	public ArrayList<Shape> getShapes() {
+		return shapes;
+	}
+
+	public void setShapes(ArrayList<Shape> shapes) {
+		this.shapes = shapes;
+	}
+	
+
+}
