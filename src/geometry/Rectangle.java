2dc0ade85bbb6ed72de1e5d541eb2f5e3a531e59
Andreas-Nagele <Andreas.Nagele@hak-reutte.ac.at>
2024-01-16 09:18:20 +0100

    Create class Rectangle
    Implement calculateCircumfence and calculateArea

---
+++ src/geometry/Rectangle.java
@@ -0,0 +1,40 @@
+package geometry;
+
+public class Rectangle extends Shape{
+
+	private double length;
+	private double width;
+	
+	public Rectangle(double length, double width) {
+		super();
+		this.length = length;
+		this.width = width;
+	}
+	
+	public double getLength() {
+		return length;
+	}
+
+	public void setLength(double length) {
+		this.length = length;
+	}
+
+	public double getWidth() {
+		return width;
+	}
+
+	public void setWidth(double width) {
+		this.width = width;
+	}
+
+	@Override
+	public double calcuateCircumfence() {
+		return 2 * (this.length + this.width);
+	}
+
+	@Override
+	public double calculateArea() {
+		return this.length * this.width;
+	}
+
+}
