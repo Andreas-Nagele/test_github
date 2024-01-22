61834b0b00a7209d9729fa3d938a25ac5f6dd113
Andreas-Nagele <Andreas.Nagele@hak-reutte.ac.at>
2024-01-22 10:10:15 +0100

    Add the Shapes to ArrayList and try out calculateSumArea

--- src/geometry/Test.java
+++ src/geometry/Test.java
@@ -3,6 +3,8 @@ package geometry;
 public class Test {
 
 	public static void main(String[] args) {
+		Group g = new Group();
+		
 		Shape r1 = new Rectangle(1, 1, 2, 4);
 		Shape c1 = new Circle (2, 2, 5);
 		Shape t1 = new Triangle(3, 3, 2, 3, 4, 5);
@@ -15,7 +17,12 @@ public class Test {
 		
 		System.out.println(t1.calcuateCircumfence());
 		System.out.println(t1.calculateArea());
+		
+		g.addShape(r1);
+		g.addShape(c1);
+		g.addShape(t1);
 
+		System.out.println("Gesamtfläche: " + g.calculateSumArea());
 	}
 
 }
