5a6889a04509f393fdc6bc9c9a4885394c1e7115
Andreas-Nagele <Andreas.Nagele@hak-reutte.ac.at>
2024-01-29 11:39:45 +0100

    Change constructor

--- src/socialNetwork/Newscast.java
+++ src/socialNetwork/Newscast.java
@@ -7,12 +7,11 @@ public class Newscast extends News {
 	
 	private String textMessage;
 
-	public Newscast(String author, ArrayList<String> comments,
-			String textMessage) {
+	public Newscast(String author, String textMessage) {
 		this.setAuthor(author);
 		this.setTimestamp(LocalDate.now());
 		this.setLikes(0);
-		this.setComments(comments);
+		this.setComments(null);
 		this.textMessage = textMessage;
 	}
 
