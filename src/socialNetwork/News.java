1c8cd39ca3ff61c63de50ce2aeb5f66ab8d34b09
Andreas-Nagele <Andreas.Nagele@hak-reutte.ac.at>
2024-01-29 11:11:33 +0100

    Add Getter/Setter

--- src/socialNetwork/News.java
+++ src/socialNetwork/News.java
@@ -10,5 +10,29 @@ public abstract class News {
 	private int likes;
 	private ArrayList <String> comments;
 	
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public LocalDate getTimestamp() {
+		return timestamp;
+	}
+	public void setTimestamp(LocalDate timestamp) {
+		this.timestamp = timestamp;
+	}
+	public int getLikes() {
+		return likes;
+	}
+	public void setLikes(int likes) {
+		this.likes = likes;
+	}
+	public ArrayList<String> getComments() {
+		return comments;
+	}
+	public void setComments(ArrayList<String> comments) {
+		this.comments = comments;
+	}
 	
 }
