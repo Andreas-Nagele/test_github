bb3c3c3f78b783f48d0ef62af0b7e783cfe08ba7
Andreas-Nagele <Andreas.Nagele@hak-reutte.ac.at>
2024-01-30 08:50:32 +0100

    add search method

--- src/socialNetwork/NewsFeed.java
+++ src/socialNetwork/NewsFeed.java
@@ -27,5 +27,15 @@ public class NewsFeed {
 	public void remove(News n) {
 		this.news.remove(n);
 	}
+	
+	public void search(String author) {
+		ArrayList <News> searchByAuthor = new ArrayList<News>();
+		for (News n : this.news) {
+			if (n.getAuthor().equals(author)) {
+				searchByAuthor.add(n);
+			}
+		}
+		System.out.println(searchByAuthor);
+	}
 
 }
