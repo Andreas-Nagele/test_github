6c1db67bd6c2db9ff83bbe219a028daef241477d
Andreas-Nagele <Andreas.Nagele@hak-reutte.ac.at>
2024-01-30 09:43:39 +0100

    do some testing

--- src/socialNetwork/Test.java
+++ src/socialNetwork/Test.java
@@ -1,21 +1,26 @@
 package socialNetwork;
 
+
 public class Test {
 
 	public static void main(String[] args) {
 		NewsFeed nf = new NewsFeed();
+		
 		News n = new Newscast("Andreas", "Das ist ein Text");
 		News p = new Photocast("Andreas", "image1.png", "Klassenfoto");
 		
 		nf.add(n);
 		nf.add(p);
 		
-		System.out.println(n);
-		System.out.println(p);
+		//nf.search("Andreas");
 		
-		nf.search("Andreas");
+		//nf.print();
+		n.addLikes();
+		p.addLikes();
+		p.addLikes();
 		
-
+		nf.print();
+	
 	}
 
 }
