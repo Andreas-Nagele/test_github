4854680387c11cb0bf20184e010834c003d88231
Andreas-Nagele <Andreas.Nagele@hak-reutte.ac.at>
2024-02-06 09:21:37 +0100

    Add Getter/Setter, constructor and toString

---
+++ src/socialNetwork/Commentary.java
@@ -0,0 +1,35 @@
+package socialNetwork;
+
+public class Commentary {
+	
+	private String text;
+	private int likes;
+	
+	public Commentary(String text) {
+		this.text = text;
+		this.likes = 0;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public int getLikes() {
+		return likes;
+	}
+
+	public void setLikes(int likes) {
+		this.likes = likes;
+	}
+	
+	public String toString() {
+		return "Kommentar: \n" + 
+				"\t Text: " + this.text + "\n" +
+				"\t Anzahl Likes: " + this.likes + "\n";
+				
+	}
+}
