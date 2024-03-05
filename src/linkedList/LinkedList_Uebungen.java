4a2a512fc3ef5a1ff9e98cc6b13a97f97ffc6883
Andreas-Nagele <Andreas.Nagele@hak-reutte.ac.at>
2024-03-05 08:42:15 +0100

    Create iterator

--- src/linkedList/LinkedList_Uebungen.java
+++ src/linkedList/LinkedList_Uebungen.java
@@ -1,9 +1,23 @@
 package linkedList;
 
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
 public class LinkedList_Uebungen {
 
 	public static void main(String[] args) {
-
+		
+		List<Integer> list = new LinkedList<Integer>();
+		list.add(1);
+		list.add(2);
+		list.add(3);
+		
+		// "Laufvariable" zum iterieren einer Datenstruktur
+		// läuft immer vom Anfang bis zum Ende
+		Iterator<Integer> iterator = list.iterator();
+		while(iterator.hasNext()) {
+			System.out.println(iterator.next());
+		}	
 	}
-
 }
