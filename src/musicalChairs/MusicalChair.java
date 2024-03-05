5c81479d916acabdbb3cfdb4f24aa831b0b66c8a
Andreas-Nagele <Andreas.Nagele@hak-reutte.ac.at>
2024-03-05 09:35:00 +0100

    Correct a mistake

--- src/musicalChairs/MusicalChair.java
+++ src/musicalChairs/MusicalChair.java
@@ -1,37 +1,36 @@
 package musicalChairs;
-
+ 
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
-
+ 
 public class MusicalChair {
-	
-	private List <String> names;
-
+ 
+	private List<String>names;
+ 
 	public MusicalChair(String... names) {
-		// String ... kann beliebig viele Strings übernehmen
+		// String... kann beliebig viele Strings übernehmen
 		// Java regelt das intern mit einem Array
-		// dadurch wird length und nich size verwendet!
+		// dadurch wird length und nicht size verwendet!
 		if(names.length > 0) {
-			// Array.asList(names) - wandelt ein Array in eine Liste um
-			this.names = new LinkedList <String>(Arrays.asList(names));
+			// Arrays.asList(names) - wandelt ein Array in eine Liste um
+			this.names = new LinkedList<String>(Arrays.asList(names));
 		}
 	}
-	
+
 	public List<String> getNames() {
 		return names;
 	}
-
+ 
 	public void setNames(List<String> names) {
 		this.names = names;
-	}
-	
+	}	
 	public String toString() {
 		Iterator<String> itr = this.names.iterator();
 		String tmp = "";
 		while(itr.hasNext()) {
-			tmp = tmp + itr.hasNext() + ", ";
+			tmp = tmp + itr.next() + ", ";
 		}
 		return tmp;
 	}
