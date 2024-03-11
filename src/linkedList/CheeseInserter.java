28b6c4907e9c41f53ac0d22f8379119d31426252
Andreas-Nagele <Andreas.Nagele@hak-reutte.ac.at>
2024-03-11 11:44:19 +0100

    Begin with task

---
+++ src/linkedList/CheeseInserter.java
@@ -0,0 +1,24 @@
+package linkedList;
+
+import java.util.List;
+import java.util.ListIterator;
+import java.util.regex.Pattern;
+
+public class CheeseInserter {
+
+	private static final Pattern vegetables = Pattern.compile(
+			"Zucchini | Paprikas? | Zwiebeln? | Tomaten?");
+	
+	public static void insertCheeseAroundVegetable(List<String> ingredients) {
+		// Initalisierung Laufvariable (iterator)
+		// Abbruchbedingung (hasnext())
+		// keine Inkrementierung -> passiert im Schleifenkörper
+		for(ListIterator<String> iterator = ingredients.listIterator(); iterator.hasNext();) {
+			String ingredient = iterator.next();
+		}
+	}
+	
+	public static void main(String[] args) {
+
+	}
+}
