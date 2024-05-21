312895654e10e49e20cf0b14ab2d30bf677ca56b
Andreas-Nagele <Andreas.Nagele@hak-reutte.ac.at>
2024-05-21 12:21:17 +0200

    Create second electronic device

--- src/exceptions/ElectronicDeviceTest.java
+++ src/exceptions/ElectronicDeviceTest.java
@@ -5,7 +5,17 @@ public class ElectronicDeviceTest {
 	public static void main(String[] args) {
 		ElectronicDevice ed = new ElectronicDevice();
 		ed.setName("Radio");
-		ed.setWatt(100);
+		try {
+			ed.setWatt(-100);	
+		} catch(IllegalWattException e) {
+			System.out.println("Negative Watt sind nicht erlaubt!");
+		}
+		
+		try {
+			ElectronicDevice ed1 = new ElectronicDevice("Abc", -1);
+		} catch (IllegalWattException e) {
+			System.out.println("Negative Watt sind nicht erlaubt!");
+		}
 	}
 
 }
